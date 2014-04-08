bc76d65ea6128f8570d2181e9836834921ade2e2
JBcoding <mads12bjoern@gmail.com>
2014-04-08 10:49:18 +0200

    Update login.php

--- login.php
+++ login.php
@@ -10,14 +10,15 @@
 		// form data
 		$username = trim(mysql_prep($_POST['username']));
 		$password = trim(mysql_prep($_POST['password']));
-		$hashed_password = sha1($password);
+		$salt;
+		//$hashed_password = sha1($password);
 		
 		// databasaquery - remember spaces in query!
 		if(!empty($username)) {
 			$query  = "SELECT * ";
 			$query .= "FROM user ";
 			$query .= "WHERE username = '{$username}' " ;
-			$query .= "AND password = '{$hashed_password}'";
+			//$query .= "AND password = '{$hashed_password}'";
 			$query_result = mysql_query($query);
 			
 			// If database-connection went wrong
@@ -26,27 +27,53 @@
 			}
 			
 			if(mysql_num_rows($query_result) == 1) {
-				// all is well and user authenticated
-				$message = "Du er inde!";
-				$found_user = mysql_fetch_array($query_result);
-				// save user info in session
-				$_SESSION['userid'] = $found_user['userid'];
-				$_SESSION['username'] = $found_user['username'];
-				$_SESSION['userimg'] = $found_user['userimg'];
-				header("Location: createpost.php");
-				exit;
+				$salt = $found_user['salt'];
+				////////////////////////////////////////////
+				$password = trim(mysql_prep($_POST['password']));
+				$hashed_password = sha1($password . $salt);
+		
+				// databasaquery - remember spaces in query!
+				if(!empty($username)) {
+					$query  = "SELECT * ";
+					$query .= "FROM user ";
+					$query .= "WHERE username = '{$username}' " ;
+					$query .= "AND password = '{$hashed_password}'";
+					$query_result = mysql_query($query);
+			
+					// If database-connection went wrong
+					if (!$query_result) {
+						die("Databaseforespørgsel gik ikke så godt: " . mysql_error());
+					}
+			
+					if(mysql_num_rows($query_result) == 1) {
+						// all is well and user authenticated
+						$message = "Du er inde!";
+						$found_user = mysql_fetch_array($query_result);
+						// save user info in session
+						$_SESSION['userid'] = $found_user['userid'];
+						$_SESSION['username'] = $found_user['username'];
+						$_SESSION['userimg'] = $found_user['userimg'];
+						header("Location: createpost.php");
+						exit;
+					} else {
+						// username & pass combination not found
+						$error = "Hmm, du må hellere prøve igen";
+					}
+				}
+		
+		///////////////////////////////////////////////////////////
 			} else {
 				// username & pass combination not found
 				$error = "Hmm, du må hellere prøve igen";
 			}
-		}
 		} else {
 			if(isset($_GET['logout']) && $_GET['logout'] == 1) {
 				$message = "Du er nu logget ud";
 			}
 			$username = "";
 			$password = "";
-		}	
+		}
+	}
 ?>
 <?php include("includes/header.php"); ?>
 
